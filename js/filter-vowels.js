d10c39152d27fdf4e9ee35596c3abe4012525530
Delaerous <77695875+Delaerous@users.noreply.github.com>
2021-03-31 16:51:45 +0300

    Add files via upload

---
+++ js/filter-vowels.js
@@ -0,0 +1,21 @@
+const string = "Привет! Как дела?Вроде получилось";
+
+const vowels = ["у", "е", "ы", "а", "о", "э", "ё", "я", "и"];
+
+const getVowels = stringToFilter => {
+  let extractedVowels = "";
+
+  for (let i = 0; i < stringToFilter.length; i++) {
+    const currentLetter = stringToFilter[i].toLowerCase();
+
+    if (vowels.includes(currentLetter)) {
+        extractedVowels += currentLetter;
+    }
+
+  }
+
+  return extractedVowels;
+}
+
+console.log(getVowels(string));
+
